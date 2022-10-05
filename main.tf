f2b7cec8d9306d3a73dc0aeba751e2e88679b5ac
Alexandros Litsakis <alexandroslitsakis@gmail.com>
2022-10-06 01:35:08 +0300

    fixing some typos

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 //terraform apply -var-file terraform-dev.tfvars
 provider "aws"{
-    region =var.region 
+  region =var.region 
 
 
 }
@@ -8,48 +8,48 @@ provider "aws"{
  
 
 resource "aws_vpc" "xe-demo-vpc"{
-    cidr_block =var.xe_vpc_cidr_block //vpc  ip address range
-    tags = {
-        Name: "${var.env_prefix}-vpc"
-    }
+  cidr_block =var.xe_vpc_cidr_block //vpc  ip address range
+  tags = {
+    Name: "${var.env_prefix}-vpc"
+  }
 
 }
 
 #gateway-subnet creator
 module "myapp-subnet"{ 
   source = "./modules/subnet"
-     xe_subnet_cidr=var.xe_subnet_cidr//subnet ip address range
-    avail_zone=var.avail_zone
-    env_prefix=var.env_prefix
-    vpc_id=aws_vpc.xe-demo-vpc.id
-    default_route_table_id=aws_vpc.xe-demo-vpc.default_route_table_id
+  xe_subnet_cidr=var.xe_subnet_cidr//subnet ip address range
+  avail_zone=var.avail_zone
+  env_prefix=var.env_prefix
+  vpc_id=aws_vpc.xe-demo-vpc.id
+  default_route_table_id=aws_vpc.xe-demo-vpc.default_route_table_id
 }
 
 # ec2 creator 
 module "webserver"{
   source = "./modules/webserver"
-      my_vpc_id=aws_vpc.xe-demo-vpc.id
-      my_ip_adress=var.my_ip_adress
-      env_prefix=var.env_prefix
-      my_public_key_loc=var.my_public_key_loc  
-      instance_type=var.instance_type
-      instance_profile=module.sqssnspairs.ec2_iam_instance.name
-      my_private_key_loc=var.my_private_key_loc
-      sqsurl=module.sqssnspairs.sns_sqs_pair[0].url
-      RedriveUrl=var.RedriveUrl
-      region=var.region
-      Timeout=var.Timeout
-      ServiceUrl=var.ServiceUrl
-      subnet_id=module.myapp-subnet.subnet.id
-      avail_zone=var.avail_zone
+  my_vpc_id=aws_vpc.xe-demo-vpc.id
+  my_ip_adress=var.my_ip_adress
+  env_prefix=var.env_prefix
+  my_public_key_loc=var.my_public_key_loc  
+  instance_type=var.instance_type
+  instance_profile=module.sqssnspairs.ec2_iam_instance.name
+  my_private_key_loc=var.my_private_key_loc
+  sqsurl=module.sqssnspairs.sns_sqs_pair[0].url
+  RedriveUrl=var.RedriveUrl
+  region=var.region
+  Timeout=var.Timeout
+  ServiceUrl=var.ServiceUrl
+  subnet_id=module.myapp-subnet.subnet.id
+  avail_zone=var.avail_zone
   
 }
 
 # creating a sns sqs pair and also create an ec2 iam profile
 module "sqssnspairs"{
   source = "./modules/sqssnspairs"
-       env_prefix=var.env_prefix
-        stream_numbers=var.stream_numbers
+  env_prefix=var.env_prefix
+  stream_numbers=var.stream_numbers
 
     
 }
--- modules/sqssnspairs/main.tf
+++ modules/sqssnspairs/main.tf
@@ -2,28 +2,55 @@
 
 #creating sns topic
 resource "aws_sns_topic" "sns-pair-1" {
-   count=var.stream_numbers
+  count=var.stream_numbers
   
-     tags = {
-        Name: "${var.env_prefix}-sns-pair-${count.index}"
-    }
+  tags = {
+    Name: "${var.env_prefix}-sns-pair-${count.index}"
+  }
 }
 
 
 #creating sqs 
 resource "aws_sqs_queue" "sqs-pair-1" {
-      count=var.stream_numbers
+  count=var.stream_numbers
 
 
-       tags = {
-        Name: "${var.env_prefix}-sqs-pair-${count.index}"
-    }
+  tags = {
+    Name: "${var.env_prefix}-sqs-pair-${count.index}"
+  }
 
 }
 
+resource "aws_sqs_queue_policy" "results_updates_queue_policy" {
+    queue_url = "${aws_sqs_queue.sqs-pair-1[count.index].id}"
+    count =var.stream_numbers
+
+    policy = <<POLICY
+  {
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.sqs-pair-1[count.index].arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.sns-pair-1[count.index].arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+
 #subscribe sns topic to sqs
 resource "aws_sns_topic_subscription" "sqs-subscription-pair-1" {
-count =var.stream_numbers
+  count =var.stream_numbers
   topic_arn = aws_sns_topic.sns-pair-1[count.index].arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.sqs-pair-1[count.index].arn
@@ -36,9 +63,9 @@ count =var.stream_numbers
 #create an ec2 role
 resource "aws_iam_role" "ec2-role" {
  
-    name= "${var.env_prefix}-ec2-role"
-    assume_role_policy = jsonencode(
-{
+  name= "${var.env_prefix}-ec2-role"
+  assume_role_policy = jsonencode(
+  {
   "Version": "2012-10-17",
   "Statement": [
     {
@@ -49,21 +76,21 @@ resource "aws_iam_role" "ec2-role" {
         }
     }
   ]
-})
+  })
 
 
- tags = {
-        Name: "${var.env_prefix}-c2-"
+   tags = {
+    Name: "${var.env_prefix}-c2-"
     }
 }
 
 #create a full rights iam policy for the sqs actions and attached to the role created before
 resource "aws_iam_role_policy" "ec2-sqs-policy" {
   count=var.stream_numbers
-    name = "${var.env_prefix}-AllowSQSPermissions${count.index}"
-    role = "${aws_iam_role.ec2-role.id}"
-    policy = <<EOF
-{
+  name = "${var.env_prefix}-AllowSQSPermissions${count.index}"
+  role = "${aws_iam_role.ec2-role.id}"
+  policy = <<EOF
+  {
   "Version": "2012-10-17",
   "Statement": [
     {
@@ -74,8 +101,8 @@ resource "aws_iam_role_policy" "ec2-sqs-policy" {
       "Resource": ["${aws_sqs_queue.sqs-pair-1[count.index].arn}"]
     }
   ]
-}
-EOF
+  }
+  EOF
 }
 
 # creates an ec2 iam profile having the the iam role s (will be as many roles as many is the sns sqs pairs)
